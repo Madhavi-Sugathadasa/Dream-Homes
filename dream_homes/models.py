24fcd6327f883507cad49b049c3fc3e46fe64843
Madhavi-Sugathadasa <madhavi.sug@gmail.com>
2020-06-09 16:54:00 -0600

    add Rent Ad Item model

--- dream_homes/models.py
+++ dream_homes/models.py
@@ -100,3 +100,34 @@ class Buy_Item_Inspection(models.Model):
     
     def __str__(self):
         return f"{self.ad_item} - {self.date} - {self.from_time} - {self.to_time}"
+    
+class Rent_Ad_Item(models.Model):
+    
+    def _image_path_(self, filename):
+        return (conf_settings.MEDIA_PATH + '/rental_properties/{0}/{1}').format(self.id, filename)
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="rent_ad_user")
+    weekly_price = models.FloatField()
+    title = models.CharField(max_length=200)
+    desc = models.CharField(max_length=4000)
+    property_type = models.ForeignKey(Property_Type, on_delete=models.DO_NOTHING, related_name="rent_ad_property_type")
+    address_line = models.CharField(max_length=200)
+    location = models.ForeignKey(Location, on_delete=models.DO_NOTHING, related_name="rent_ad_location", default="")
+    bedrooms = models.IntegerField(default=1)
+    bathrooms= models.IntegerField(default=1)
+    parking = models.IntegerField(default=1)
+    garages = models.IntegerField(default=0)
+    floorplan = models.ImageField(upload_to =_image_path_, default="", null=True, blank=True)
+    contact_name = models.CharField(max_length=64, default="")
+    email = models.CharField(max_length=64, default="")
+    mobile = models.CharField(max_length=64, default="")
+    date_time = models.DateTimeField()
+    active = models.BooleanField(default=True)
+    priority  = models.IntegerField(default=1)
+    payment = models.BooleanField(default=False)
+    payment_pkg = models.CharField(max_length=4,default="")
+    amount_paid = models.FloatField(default=0.0)
+    payemnt_session = models.CharField(max_length=200,default="")
+    
+    def __str__(self):
+        return f"{self.id} - {self.title}"
