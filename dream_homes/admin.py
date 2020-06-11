d15d5e0944e2513bcd892f7162b26cbd9ba6f02e
Madhavi-Sugathadasa <madhavi.sug@gmail.com>
2020-06-11 13:31:00 -0600

    update admin

--- dream_homes/admin.py
+++ dream_homes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Location
+from .models import Location, Property_Type
 
 # Register your models here.
 admin.site.register(Location)
+admin.site.register(Property_Type)
